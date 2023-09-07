34336614da73c5192e80650683eb46fd961b889b
dakotasmith13 <97972068+dakotasmith13@users.noreply.github.com>
2023-09-07 13:24:58 -0400

    still testing

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+    <section class="logo-image">
+        <h1 hidden>Logo</h1>
+        <a href="../final/index.php"><img src="../final/images/logo.png" alt="logo"></a>
+    </section>
+    <section class="title">
+        <a href="../final/index.php"><h1>Project: Miata</h1></a>
+    </section>
+    <section class="top-nav">
+        <h2 hidden>Top</h2>
+        <?php
+        include 'nav.php';
+        ?>
+    </section>
+</header>
